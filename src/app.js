0eccc7b0e727222211dbdf6cfcf3c808d7a93bb4
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-06 22:36:00 -0800

    Uncomments routing for 'weather'.

--- src/app.js
+++ src/app.js
@@ -24,13 +24,13 @@ app.use(express.static(publicDirectoryPath));
 //     res.send('<h1>About page.</h1>');
 // });
 
-// app.get('/weather', (req, res) => {
-//     const weather = {
-//         location: 'Longview',
-//         forecast: 'Slush'
-//     };
-//     res.send(weather);
-// });
+app.get('/weather', (req, res) => {
+    const weather = {
+        location: 'Longview',
+        forecast: 'Slush'
+    };
+    res.send(weather);
+});
 
 app.listen(3000,() => {
     console.log('Server is up on port 3000.');
