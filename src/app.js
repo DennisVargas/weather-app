cf75d58778480b94220d6d9fcd5d1f63eded4d24
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-06 22:27:52 -0800

    Comments out the nodejs responses to routes; replaced with static public folder.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 /* jshint esversion:6 */
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) =>{
-    res.send('Hello express!');
-});
+app.use(express.static(publicDirectoryPath));
 
-app.get('/help', (req, res) => {
-    res.send('Help page.');
-});
+// app.get('', (req, res) =>{
+//     res.send('<h1>Hello express!</h1>');
+// });
 
-app.get('/about', (req, res) => {
-    res.send('About page.');
-});
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'dennis',
+//         age: 37},{
+//         name: 'dens',
+//         age: 32
+//     }]);
+// });
 
-app.get('/weather', (req, res) => {
-    res.send('Weather page.');
-});
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About page.</h1>');
+// });
+
+// app.get('/weather', (req, res) => {
+//     const weather = {
+//         location: 'Longview',
+//         forecast: 'Slush'
+//     };
+//     res.send(weather);
+// });
 
 app.listen(3000,() => {
     console.log('Server is up on port 3000.');
