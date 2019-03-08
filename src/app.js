659472336445150286ae812dcb91c190c0da5464
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-07 23:58:56 -0800

    Creates routes to the handlebars files found in 'views' directory.

--- src/app.js
+++ src/app.js
@@ -4,25 +4,34 @@ const path = require('path');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
-
+const viewsDirectoryPath = path.join(__dirname,'../views');
+console.log(publicDirectoryPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsDirectoryPath);
 app.use(express.static(publicDirectoryPath));
 
-// app.get('', (req, res) =>{
-//     res.send('<h1>Hello express!</h1>');
-// });
+app.get('', (req, res) =>{
+    res.render('index',{
+        title: 'Weather',
+        name: 'Dennis Vargas'
+    });
+});
 
-// app.get('/help', (req, res) => {
-//     res.send([{
-//         name: 'dennis',
-//         age: 37},{
-//         name: 'dens',
-//         age: 32
-//     }]);
-// });
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'About Me',
+        name: 'Dennis Vargas'
+    });
+});
 
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About page.</h1>');
-// });
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        msg: 'Help is on the way!'
+    });
+});
+
+
 
 app.get('/weather', (req, res) => {
     const weather = {
