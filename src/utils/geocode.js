11afbebf7f7e9db094fe127cbc80dfb8efcbd2de
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-08 23:17:10 -0800

    Creates a utils folder with modules for getting place coordinates and weather data.

---
+++ src/utils/geocode.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6*/
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token=pk.eyJ1IjoiZHZhcmdhcy1zb2Z0d2FyZSIsImEiOiJjanN3NTJ5anUwZTU1M3lvZGQ2c25xM29pIn0.yObGVA3-uqIqGcbYHomnBA";
+    
+    request({url: url, json: true}, (error, {body}) => {
+        if(error)
+            callback("Network Error.", undefined);
+
+        else if(body.features.length <= 0){
+            callback(`Can not find location: ${address}`, undefined);
+        }else{
+            const data = {
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                location: body.features[0].place_name
+            };
+            callback(undefined, data);
+            // console.log(response.body.features[0].text);
+            // console.log(`${response.body.features[0].center}`);
+        }
+    });
+};
+
+module.exports = geocode;
