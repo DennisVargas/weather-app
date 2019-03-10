c5b38e8e1f14fcf4ae196364655d6013a388826f
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-10 03:37:01 -0700

    Refactors error checking for faulty address. Check for existance of 'features'

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,11 +4,10 @@ const request = require('request');
 const geocode = (address, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token=pk.eyJ1IjoiZHZhcmdhcy1zb2Z0d2FyZSIsImEiOiJjanN3NTJ5anUwZTU1M3lvZGQ2c25xM29pIn0.yObGVA3-uqIqGcbYHomnBA";
     
-    request({url: url, json: true}, (error, {body}) => {
+    request({url: url, json: true}, (error, {body}={}) => {
         if(error)
             callback("Network Error.", undefined);
-
-        else if(body.features.length <= 0){
+        else if(!body.features){
             callback(`Can not find location: ${address}`, undefined);
         }else{
             const data = {
