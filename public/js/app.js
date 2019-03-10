72d42eaa3db7ca66ca9c7cee2e0c99437f893035
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-10 03:40:47 -0700

    Creates a fetch request for weather data from server.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-console.log('howdy world');
+// jshint esversion:6
+
+fetch('http://localhost:3000/weather').then((response) =>{
+    response.json().then((data) =>{
+        if(data.error){
+            console.log(data.error);
+        }else{
+            console.log(data);
+        }
+    });
+});
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) =>{
+//     response.json().then((data) =>{
+//         console.log("client js data:",data);
+//     });
+// });
