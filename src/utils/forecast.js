244ece889781860276433638e642e8f1fc71739c
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-12 20:34:29 -0700

    Changes the profile picture link in about page and adds a paragraph.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) =>{
         else if(body.error){
             callback(`Can not find location at latt:${latitude}, long:${longitude}`, undefined);
         }else{
-            forecastMsg = `It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability*100}% chance of rain.`;
+            forecastMsg = `It is currently ${body.currently.temperature} degrees out. 
+            There is a ${body.currently.precipProbability*100}% chance of rain.`;
             callback(undefined, forecastMsg);
         }
     });
