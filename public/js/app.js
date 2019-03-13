3813dd7ec808c4e08a7d28bf8dddbebf3e4af94a
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-13 00:51:57 -0700

    Creates an unpacking of four messages sent from forecast into <p> body.

--- public/js/app.js
+++ public/js/app.js
@@ -4,23 +4,27 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
+const messageThree = document.querySelector('#message-3');
+const messageFour = document.querySelector('#message-4');
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault();
     messageOne.textContent = 'loading';    
     messageTwo.textContent = '';
     const location = search.value;
-    console.log(location);
+    // console.log(location);
     const weatherAddressQuery = '/weather?address='+location;
     fetch(weatherAddressQuery).then((response) =>{
         response.json().then((data) =>{
-            
             if(data.error){
                 messageOne.textContent = data.error;
             }else{
-                messageOne.textContent = `${data.location}`;
-                messageTwo.textContent = `${data.forecastMsg}`;
                 console.log(data);
+                messageOne.textContent = `Location: ${data.location}`;
+                messageTwo.textContent = `${data.forecastMsg[0]}`;
+                messageThree.textContent = `${data.forecastMsg[1]}`;
+                messageFour.textContent = `${data.forecastMsg[2]}`;
+                
             }
         });
     });
