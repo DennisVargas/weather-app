4bb74385abe40df3827f61cb11e67e5442070bc2
Dennis_Vargas <dennis.vargas@wsu.edu>
2019-03-13 00:50:20 -0700

    Creates a list of messages to be output by forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,16 @@ const forecast = (latitude, longitude, callback) =>{
         else if(body.error){
             callback(`Can not find location at latt:${latitude}, long:${longitude}`, undefined);
         }else{
-            forecastMsg = `It is currently ${body.currently.temperature} degrees out. 
-            There is a ${body.currently.precipProbability*100}% chance of rain.`;
-            callback(undefined, forecastMsg);
+            var todayHigh = body.daily.data[0].temperatureHigh;
+            var todayLow = body.daily.data[0].temperatureLow;
+            var currTemp = body.currently.temperature;
+            var rainProb = body.currently.precipProbability*100;
+
+            var currentTempMessage = `Currently: ${currTemp} F`;
+            var highLowMessage = `Today High: ${todayHigh} F| Today Low: ${todayLow} F`;
+            var chanceRainMessage = `Chance of Rain: ${rainProb}%`;
+            let forecastMsgs = [currentTempMessage, highLowMessage, chanceRainMessage];
+            callback(undefined, forecastMsgs);
         }
     });
 };
